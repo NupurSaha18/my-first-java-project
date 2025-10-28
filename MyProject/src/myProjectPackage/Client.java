e8e31350df13f29ec692ca71eefe14fa30f80a1d
NupurSaha18 <nupursaha142@gmail.com>
2025-10-28 17:19:48 +0600

    added comment line

--- MyProject/src/myProjectPackage/Client.java
+++ MyProject/src/myProjectPackage/Client.java
@@ -5,28 +5,31 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
+// client class main method implementation
 public class Client {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 5000);
             Calculator ob = (Calculator) registry.lookup("rmi://localhost/CalculatorService");
 
+// scanner class initialization for taking user input
             Scanner input = new Scanner(System.in);
 
+//for performing operation interactively
             boolean overallContinue = true;
             while (overallContinue) {
-
+//for taking user input
                 System.out.println("Enter a: ");
                 double a = input.nextDouble();
                 System.out.println("Enter b: ");
                 double b = input.nextDouble();
-
+//choosing operation
                 boolean pairContinue = true;
                 while (pairContinue) {
                     System.out.println("1.Add  2.Subtract  3.Multiply  4.Divide  5.Change a & b  6.Exit");
                     System.out.println("Enter choice:");
                     int choice = input.nextInt();
-
+//switch to choose operation
                     switch (choice) {
                         case 1:
                             System.out.println("Result: " + ob.add(a, b));
